47a7723bdb157cfe4e1a1f29690d37336bf7b8c7
andygimma <andrewgimma@protonmail.com>
2019-10-15 04:42:01 -0400

    Improve graph

--- src/test.py
+++ src/test.py
@@ -6,11 +6,12 @@ from functools import reduce
 
 
 class SunlightCalculator:
-    def __init__(self, lat=0, day=1, year=2019, num_years=10):
+    def __init__(self, lat=0, day=1, year=2019, num_years=10, city_name=None):
         self.lat = lat
         self.day = day
         self.year = year
         self.num_years = num_years
+        self.city_name = city_name
         self.temps = []
 
     def _daily_insolation(self, day):
@@ -34,16 +35,23 @@ class SunlightCalculator:
     def plot_graph_for_year(self):
         if (len(self.temps) == 0):
             self.calculate_temps_for_year()
-        plt.plot(range(1, 366), self.temps)
+        average = self.calculate_yearly_average()
+        plt.plot(range(1, 366), self.temps, label="Daily temps")
+        # plt.plot(range(1, 366), self.calculate_yearly_average())
+        label = "Average: " + str(round(average, 2))
+        plt.axhline(average, color='blue', linewidth=2, label=label)
         plt.xlabel('Day of year')
         plt.ylabel('Insolation')
         plt.ylim([0, 600])
-        title = "Yearly insolation at " + str(self.lat) + " degrees latitude"
+        title = "Yearly insolation at " + \
+            str(self.lat) + " degrees latitude " + self.city_name
         plt.title(title)
+        plt.legend()
         plt.show()
 
     def calculate_yearly_average(self):
-        self.calculate_temps_for_year()
+        if (len(self.temps) == 0):
+            self.calculate_temps_for_year()
         acc = reduce((lambda x, y: x + y), self.temps)
         return acc / len(self.temps)
 
@@ -51,7 +59,9 @@ class SunlightCalculator:
         pass
 
 
-calc = SunlightCalculator(lat=6.3, day=172, year=2019, num_years=20)
-print("Average: ", calc.calculate_temps_for_year())
+calc = SunlightCalculator(lat=52.5, day=172, year=2019,
+                          num_years=20, city_name="Berlin, Germany")
+# print("Average: ", calc.calculate_temps_for_year())
 print("Daily: ", calc.plot_graph_for_year())
-print("calculate_daily_average", calc.calculate_daily_average(1))
+# print("calculate_daily_average", calc.calculate_daily_average(1))
+# print("calculate_yearly_average", calc.calculate_yearly_average())
