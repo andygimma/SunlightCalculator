ab0f48f4b53e58f454f761adc279d595623cdccf
andygimma <andrewgimma@protonmail.com>
2019-10-15 04:18:20 -0400

    Change get_ to calculate_

--- src/test.py
+++ src/test.py
@@ -18,22 +18,22 @@ class SunlightCalculator:
         orb = OrbitalTable.interp(kyear=years)
         return daily_insolation(lat=self.lat, day=day, orb=orb)
 
-    def get_daily_average(self, day):
+    def calculate_daily_average(self, day):
         insolation_data = self._daily_insolation(day)
         acc = reduce((lambda x, y: x + y.to_dict()['data']), insolation_data)
         average = acc.to_dict()['data'] / len(insolation_data)
         return average
 
-    def get_temps_for_year(self):
+    def calculate_temps_for_year(self):
         self.temps = []
         for day in range(1, 366):
-            avg = self.get_daily_average(day)
+            avg = self.calculate_daily_average(day)
             print("day: ", day, avg)
             self.temps.append(avg)
 
     def plot_graph_for_year(self):
         if (len(self.temps) == 0):
-            self.get_temps_for_year()
+            self.calculate_temps_for_year()
         plt.plot(range(1, 366), self.temps)
         plt.xlabel('Day of year')
         plt.ylabel('Insolation')
@@ -42,16 +42,16 @@ class SunlightCalculator:
         plt.title(title)
         plt.show()
 
-    def get_yearly_average(self):
-        self.get_temps_for_year()
+    def calculate_yearly_average(self):
+        self.calculate_temps_for_year()
         acc = reduce((lambda x, y: x + y), self.temps)
         return acc / len(self.temps)
 
-    def get_daily_averages_for_multiple_years(self, lat, num_years):
+    def calculate_daily_averages_for_multiple_years(self, lat, num_years):
         pass
 
 
 calc = SunlightCalculator(lat=6.3, day=172, year=2019, num_years=20)
-# print("Average: ", calc.get_temps_for_year())
-# print("Daily: ", calc.plot_graph_for_year())
-print("get_daily_average", calc.get_daily_average(1))
+print("Average: ", calc.calculate_temps_for_year())
+print("Daily: ", calc.plot_graph_for_year())
+print("calculate_daily_average", calc.calculate_daily_average(1))
